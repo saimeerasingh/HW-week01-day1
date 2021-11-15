8effc5e5a1aa18a0ea0daf20bf0771f6e5e9de4a
saimeerasingh <singhsaimeera@gmail.com>
2021-11-15 22:51:39 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,41 @@
+# SET day = input("Whats the day today?")
+# IF Monday
+#    PRINT " water the monstera"
+# ELIF Tuesday
+#    PRINT "water the arrow head"
+# ELIF Wednesday
+#    PRINT "water the peace lily"
+# ELIF Thursday
+#    PRINT "Water the string of hearts"
+# ELIF Friday
+#    PRINT "pump plant food"
+# ELIF Saturday
+#    Print "water the herbs"
+# ELSE Sunday
+#    Print "Take rest"
+
+
+day = input("Whats the day today? ")
+
+if day == "Monday":
+    print("Water the monstera")
+
+elif day == "Tuesday":
+    print("Water the arrow head")
+
+elif day == "Wednesday":
+    print("Water the peace lily")
+
+elif day == "Thursday":
+    print("Water the string of hearts")
+
+elif day == "Friday":
+    print("Pump some plant food")
+
+elif day == "Saturday":
+    print("Water the herbs")
+
+else:
+    print("Take rest")
+
+print("Thanks!")    
